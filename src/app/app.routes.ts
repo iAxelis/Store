8ea29af81867afeae8da7029ef208a07c4a46a42
iAxelis <elaxelis701@gmail.com>
2026-05-20 08:55:55 -0600

    feat (admin):se agrego dasboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/Home/Home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    
+    {
+        path: 'home',
+        component: Home
+
+    },
+    {
+        path:"**",
+        redirectTo:"/home",
+        pathMatch:"full"
+    }
+
+];

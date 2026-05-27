b321a95fa53c351f0a200806e5627522abde91c1
XaviVal0 <xaviermateos23@gmail.com>
2026-05-27 08:34:13 -0600

    feat:Se agrego newProduct

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,20 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/Home/Home';
+import { Products } from '../pages/products/products';
+import { NewProduct } from '../pages/NewProduct/NewProduct';
 
 export const routes: Routes = [
-    
+
     {
         path: 'home',
         component: Home
+    },{
+        path: 'products',
+        component: Products
     },
     {
-        path 'products'
-        component : Products
+        path: 'newProduct',
+        component: NewProduct
     },
     {
         path:"**",
--- src/pages/products/products.ts
+++ src/pages/products/products.ts
@@ -1,10 +1,24 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NewProduct } from '../NewProduct/NewProduct';
 
 @Component({
   selector: 'app-products',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule,NewProduct],
   templateUrl: './products.html',
   styleUrl: './products.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Products {}
+export class Products {
+  // Estado reactivo para saber si el modal está abierto o cerrado
+  isModalOpen = signal(false);
+
+  openModal() {
+    this.isModalOpen.set(true);
+  }
+
+  closeModal() {
+    this.isModalOpen.set(false);
+  }
+}
